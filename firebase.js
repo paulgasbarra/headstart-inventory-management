02830cd368170c6dbb847f5886f3c785fd2ad276
Paul Gasbarra <paulgasbarra@admins-air.lan>
2024-08-12 16:36:30 -0400

    add a .env file

--- firebase.js
+++ firebase.js
@@ -1,19 +1,6 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDLpeAfkjxmMybNnPV5tV5K8Yw3fb5lAuc",
-//     authDomain: "inventory-management-app-d4410.firebaseapp.com",
-//     projectId: "inventory-management-app-d4410",
-//     storageBucket: "inventory-management-app-d4410.appspot.com",
-//     messagingSenderId: "168090410116",
-//     appId: "1:168090410116:web:ef3070946e8080125876d1",
-//     measurementId: "G-MWNXYNDMNQ"
-//   };
-// const app = initializeApp(firebaseConfig);
-// const firestore = getFirestore(app);
-// export { firestore };
 
-// Import the functions you need from the SDKs you need
+require('dotenv').config()
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
@@ -23,8 +10,8 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLpeAfkjxmMybNnPV5tV5K8Yw3fb5lAuc",
-  authDomain: "inventory-management-app-d4410.firebaseapp.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "inventory-management-app-d4410",
   storageBucket: "inventory-management-app-d4410.appspot.com",
   messagingSenderId: "168090410116",
@@ -36,3 +23,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export {app, firestore}
+
