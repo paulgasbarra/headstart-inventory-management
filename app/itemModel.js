00eaecd99bdeeb26f3f4f4e8aa9297020a15f0ce
Paul Gasbarra <paulgasbarra@admins-air.lan>
2024-08-12 17:06:53 -0400

    fix linting error

--- app/itemModel.js
+++ app/itemModel.js
@@ -1,14 +1,14 @@
-{
-    "itemName": "Canned Beans",
-    "quantity": 3,
-    "unitOfMeasure": "cans",
-    "category": "Canned Goods",
-    "location": "Pantry Shelf 2",
-    "barcode": "123456789012",
-    "nutritionalInformation": {
-      "calories": 150,
-      "protein": "7g",
-      "fat": "1g",
-      "carbohydrates": "30g"
-    }
-  }
+// {
+//     "itemName": "Canned Beans",
+//     "quantity": 3,
+//     "unitOfMeasure": "cans",
+//     "category": "Canned Goods",
+//     "location": "Pantry Shelf 2",
+//     "barcode": "123456789012",
+//     "nutritionalInformation": {
+//       "calories": 150,
+//       "protein": "7g",
+//       "fat": "1g",
+//       "carbohydrates": "30g"
+//     }
+//   }
